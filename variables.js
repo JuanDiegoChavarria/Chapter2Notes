3a2b557ae4e0b75a2ee88eaf02e1c4236017fbd5
98h7 <98h7@fresnou.org>
2023-02-06 13:48:34 -0800

    initial commit

---
+++ variables.js
@@ -0,0 +1,61 @@
+//variables store data for later use
+
+//create a variable with let
+let myVariable = "value";
+let mynewVariable = 7;
+let myBool = true;
+
+//accessing variable values
+console.log(myVariable)
+console.log(mynewVariable)
+console.log(myBool)
+
+//update value stored in variable
+mynewVariable=10;
+console.log(mynewVariable)
+
+//increase by 1
+mynewVariable = mynewVariable+1
+console.log(mynewVariable)
+mynewVariable = mynewVariable-1
+
+
+//multiply by 3
+mynewVariable = mynewVariable * 3
+console.log(mynewVariable)
+
+//divide and return the reminder
+mynewVariable = mynewVariable % 2;
+console.log(mynewVariable)
+
+//shortcuts
+
+//add one
+let x = 7;
+x++;   //x=x+1
+
+
+//subject one
+let y = 17;
+y--;   //y=y-1
+
+let z = 10;
+z+=5   //z=z+5
+
+let a = 10;
+a-=2   //a=a-2
+
+let c = 10;
+c/=2   //c=c/2
+
+let b = 10;
+b/=2   //b=b*2
+
+//keywords
+const pi = 3.14;
+const username = "Msbolin";
+//constants cannot be changed
+
+var old = 123;
+//var is the old way to create variables
+//DON'T USE IT. use let instead
