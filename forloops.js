01dc6c114276299e12b89d81eb0114f34df2868d
98h7 <98h7@fresnou.org>
2023-02-23 14:18:26 -0800

    initial commit

---
+++ forloops.js
@@ -0,0 +1,101 @@
+//for loops
+// for (let i = 0; i <= 10; i++ ){
+//     console.log("hi");
+// }
+
+// for (let i = 0; i <= 10; i++ ){
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 1000; i++ ){
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 40; i++ ){
+//     console.log(i * i);
+// }
+
+// for (let i = 100; i <= 300; i++ ) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++){
+//     if (i % 2 == 1)
+//         sum += 1;
+// }
+// console.log(sum);
+
+// 1
+// for (let i = 0; i <= 150; i++ ){
+//     console.log(i);
+// }
+
+
+//2
+// for (let i = 1; i <= 40; i++ ){
+//     console.log(i * i);
+// }
+
+//3
+// for (let i = 1; i <= 40; i++ ){
+//     console.log(i * i * 1);
+// }
+
+//4
+// for (let i = 0; i <= 700; i++ ) {
+//     if (i % 5 == 0) {
+//     console.log(i);
+// }
+// }
+
+//5
+// for (let i = 350; i <= 1000; i++ ) {
+//     if (i % 10 == 0) {
+//         console.log(i);
+//     }
+// }
+
+//6
+// let sum = 0;
+// for (let i = 0; i <= 50; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+//7
+// let sum = 0;
+// for (let i = 35; i <= 70; i++){
+//     sum += i;
+// }
+// console.log(sum);
+//
+
+//8
+// let product = 1;
+// for (let i = 1; i <= 15; i++){
+//     product *= i;
+// }
+// console.log(product);
+
+//9
+// let product = 1;
+// for (let i = 10; i <= 35; i++){
+//     product *= i;
+// }
+// console.log(product);
+
+//10
+// for (let i = 0; i <= 12; i++ ){
+//     console.log("Happy Holidays");
+// }
+
+
---
+++ whileloops1.js
@@ -0,0 +1,139 @@
+const readline = require('readline-sync');
+
+//1
+// let x = 0;
+// while (x<26){
+//     console.log(x);
+//     x++;
+// }
+
+//2
+// let x = 0;
+// while (x<51){
+//     console.log(x);
+//     x++;
+// }
+
+//3
+// let x = -10;
+// while (x<11){
+//     console.log(x);
+//     x++;
+// }
+
+//4
+// let x = -50;
+// while (x<0){
+//     console.log(x);
+//     x++;
+// }
+
+// 5
+// let x = 0;
+// while (x<101){
+//     if(x % 2 == 0) {
+//         console.log(x);
+//     }
+//     x++;
+// }
+
+//6
+// let x = 0;
+// while (x<101){
+//     if(x % 2 == 1) {
+//         console.log(x);
+//     }
+//     x++;
+// }
+
+//7
+// let x = 0;
+// while (x<101){
+//     if(x % 3 == 0) {
+//         console.log(x);
+//     }
+//     x++;
+// }
+
+//8
+// let x = 0;
+// while (x<1001){
+//     if(x % 5 == 0) {
+//         console.log(x);
+//     }
+//     x++;
+// }
+
+//9
+// let start = 0;
+// while(start <= 100){
+//     if (start < 10) {
+//         console.log(start);
+//     }else if (start > 26){
+//         console.log(start)
+//     }
+//     start++;
+// }
+
+//10
+// let start = 0;
+// while(start <= 100){
+//     if (start < 10) {
+//         console.log(start);
+//     }else if (39>start > 21){
+//         console.log(start)
+//     }else if (51<start){
+//         console.log(start)
+//     }
+//     start++;
+// }
+
+// let answer = readline.question("do you want to quit ?\nEnter y or n: ");
+// while (answer != "Y" && answer != "N"){
+//     console.log("Invalid Choice.")
+//     answer = readline.question("Try again. Enter y or n: ");
+// }
+// console.log("End Of Program")
+
+// let answer = readline.question("Enter 1,2,3: ");
+// while (answer != "1" && answer != "2" && answer!= "3"){
+//     console.log("Invalid Choice.")
+//     answer = readline.question("Try again\nEnter 1,2,3:  ");
+// }
+// console.log("End Of Program")
+
+// let answer = readline.question("Give me a number between 0 and 10: ");
+// while (answer != "0" && answer != "1" && answer != "2" && answer!= "3" && answer != "4" && answer != "5" && answer!= "6" && answer != "7" && answer!= "8" && answer != "9" && answer != "10"){
+//     console.log("Invalid Choice")
+//     answer = readline.question("Try again.\nGive me a number between 0 and 10:  ");
+// }
+// console.log("End Of Program")
+
+// let x = readline.question("Enter any positive number: ");
+// while (x < 0){
+//     console.log("Invalid Choice")
+//     x = readline.question("Try again. Enter y or n: ");
+// }
+// console.log("End Of Program")
+
+// let x = readline.question("Enter any negative number: ");
+// while (x > 0){
+//     console.log("Invalid Choice")
+//     x = readline.question("Try again. Enter y or n: ");
+// }
+// console.log("End Of Program")
+
+// let x = readline.question("Enter any even number: ");
+// while (x % 2 == 1){
+//     console.log("Invalid answer")
+//     x = readline.question("Try again. Enter y or n: ");
+// }
+// console.log("End Of Program")
+
+// let x = readline.question("Enter any even number between 0-10 or 20-30: ");
+// while (x < 0 && x > 10 && 20 && x > 30){
+//     console.log("Invalid answer")
+//     x = readline.question("Try again\n Enter any even number between 0-10 or 20-30:");
+// }
+// console.log("End Of Program")
+
