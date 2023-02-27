03c51d8eaf23c2a071de9beac8496d22e3b6af9b
98h7 <98h7@fresnou.org>
2023-02-27 13:46:29 -0800

    initial commit

---
+++ whileloops2.js
@@ -0,0 +1,95 @@
+const readline = require('readline-sync');
+
+//11/12
+// let answer = readline.question("do you want to quit ?\nEnter y or n: ");
+// while (answer != "Y" && answer != "N"){
+//     console.log("Invalid Choice.")
+//     answer = readline.question("Try again. Enter y or n: ");
+// }
+// console.log("End Of Program")
+
+//13
+// let answer = readline.question("Enter A,B,C: ");
+// while (answer != "A" && answer != "B" && answer!= "C"){
+//     console.log("Invalid Choice.")
+//     answer = readline.question("Try again\nEnter A,B,C:  ");
+// }
+// console.log("End Of Program")
+
+//14
+// let answer = readline.question("Enter 1,2,3: ");
+// while (answer != "1" && answer != "2" && answer!= "3"){
+//     console.log("Invalid Choice.")
+//     answer = readline.question("Try again\nEnter 1,2,3:  ");
+// }
+// console.log("End Of Program")
+
+//15
+// let answer = readline.question("Give me a number between 0 and 10: ");
+// while (answer != "0" && answer != "1" && answer != "2" && answer!= "3" && answer != "4" && answer != "5" && answer!= "6" && answer != "7" && answer!= "8" && answer != "9" && answer != "10"){
+//     console.log("Invalid Choice")
+//     answer = readline.question("Try again.\nGive me a number between 0 and 10:  ");
+// }
+// console.log("End Of Program")
+
+//16
+// let x = readline.question("Enter any positive number: ");
+// while (x < 0){
+//     console.log("Invalid Choice")
+//     x = readline.question("Try again. Enter y or n: ");
+// }
+// console.log("End Of Program")
+
+
+//17
+// let x = readline.question("Enter any negative number: ");
+// while (x > 0){
+//     console.log("Invalid Choice")
+//     x = readline.question("Try again. Enter y or n: ");
+// }
+// console.log("End Of Program")
+
+//18
+// let x = readline.question("Enter any even number: ");
+// while (x % 2 == 1){
+//     console.log("Invalid answer")
+//     x = readline.question("Try again. Enter y or n: ");
+// }
+// console.log("End Of Program")
+
+//19
+// let x = readline.question("Enter any even number between 0-10 or 20-30: ");
+// while (x < 0 && x > 10 && 20 && x > 30){
+//     console.log("Invalid answer")
+//     x = readline.question("Try again\n Enter any even number between 0-10 or 20-30:");
+// }
+// console.log("End Of Program")
+
+//20
+// let answer = readline.question("Choose A Month #: ");
+//  if (answer=='1'){
+//     console.log("There are 31 days")
+// }if (answer=='2'){
+//     console.log("There are 28 days")
+// }if (answer=='3'){
+//     console.log("There are 31 days")
+// }if (answer=='4'){
+//     console.log("There are 30 days")
+// }if (answer=='5'){
+//     console.log("There are 31 days")
+// }if (answer=='6'){
+//     console.log("There are 30 days")
+// }if (answer=='7'){
+//     console.log("There are 31 days")
+// }if (answer=='8'){
+//     console.log("There are 31 days")
+// }if (answer=='9'){
+//     console.log("There are 30 days")
+// }if (answer=='10'){
+//     console.log("There are 31 days")
+// }if (answer=='11'){
+//     console.log("There are 30 days")
+// }if (answer=='12'){
+//     console.log("There are 31 days")
+// }
+
